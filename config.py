0c542f44715babe989e6c24084c8447d0975eceb
XenHunt <95288387+XenHunt@users.noreply.github.com>
2024-10-10 21:37:03 +0300

    Chnages:started rest-api part

---
+++ config.py
@@ -0,0 +1,24 @@
+import os
+
+
+def getEnv(key):
+    envval = os.getenv(key)
+    if envval is None:
+        return "None"
+    return envval
+
+
+config = {
+    # "BOT_TOKEN": getEnv("BOT_TOKEN"),
+    # "WEATHER_TOKEN": getEnv("WEATHER_TOKEN"),
+    "DATA_BASE": {
+        "IP": getEnv("DATABASE_IP"),
+        "NAME": getEnv("DATABASE_NAME"),
+        "USER": getEnv("DATABASE_USER"),
+        "PORT": getEnv("DATABASE_PORT"),
+        "PASSWORD": getEnv("DATABASE_PASSWORD"),
+    },
+}
+
+if __name__ == "__main__":
+    print(config)
